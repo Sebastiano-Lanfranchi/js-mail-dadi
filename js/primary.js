58d35fc77237c3886db2a32f46701cd238ff36fd
Sebastiano-Lanfranchi <sebastiano.lanfranchi@gmail.com>
2020-12-15 15:33:01 +0100

    esercizio finito + piccolo bug

--- js/primary.js
+++ js/primary.js
@@ -12,4 +12,19 @@ else {
 
 // Esercizio 2
 
-var 
+var sceltaUtente = prompt('inserisci un numero da 1 a 6');
+var sceltaPc = Math.floor(Math.random() * 5) +1;
+document.getElementById('scelta1').innerText = 'La tua scelta:' + sceltaUtente;
+document.getElementById('scelta2').innerText = 'La scelta del pc:' + sceltaPc;
+if (sceltaUtente > sceltaPc){
+ document.getElementById('outputDadi').innerText = 'Hai vinto'
+};
+if (sceltaUtente > sceltaPc) {
+  document.getElementById('outputDadi').innerText = 'Hai perso'
+};
+if (sceltaUtente === sceltaPc){
+  document.getElementById('outputDadi').innerText = 'Hai pareggiato'
+};
+if(sceltaUtente === 0 || sceltaUtente > 6){
+  document.getElementById('outputDadi').innerText = 'Non barare e non rompermi il codice!'
+};
