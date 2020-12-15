408a94f714b6938c1624510ed7f1ebe91ee97632
Sebastiano-Lanfranchi <sebastiano.lanfranchi@gmail.com>
2020-12-15 16:11:16 +0100

    Esercizio finito

--- js/primary.js
+++ js/primary.js
@@ -3,6 +3,7 @@
 var mail = prompt('Inserisci la tua email');
 var iscritti = ['sebastiano.lanfranchi@gmail.com','qwerty@gmail.com'];
 
+
  if (iscritti.includes(mail)){
    document.getElementById('outputEmail').innerText = 'Accesso completato'
  }
@@ -12,19 +13,19 @@ else {
 
 // Esercizio 2
 
-var sceltaUtente = prompt('inserisci un numero da 1 a 6');
-var sceltaPc = Math.floor(Math.random() * 5) +1;
+var sceltaUtente = parseInt(prompt('inserisci un numero da 1 a 6'));
+var sceltaPc = parseInt(Math.floor(Math.random() * 5) +1);
 document.getElementById('scelta1').innerText = 'La tua scelta:' + sceltaUtente;
 document.getElementById('scelta2').innerText = 'La scelta del pc:' + sceltaPc;
 if (sceltaUtente > sceltaPc){
  document.getElementById('outputDadi').innerText = 'Hai vinto'
-};
-if (sceltaUtente > sceltaPc) {
+}
+if (sceltaUtente < sceltaPc) {
   document.getElementById('outputDadi').innerText = 'Hai perso'
-};
+}
 if (sceltaUtente === sceltaPc){
   document.getElementById('outputDadi').innerText = 'Hai pareggiato'
-};
+}
 if(sceltaUtente === 0 || sceltaUtente > 6){
   document.getElementById('outputDadi').innerText = 'Non barare e non rompermi il codice!'
-};
+}
