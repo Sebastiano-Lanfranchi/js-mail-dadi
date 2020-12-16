21697d77e58e04f2f4f86171753bcc35f00e897f
Sebastiano-Lanfranchi <sebastiano.lanfranchi@gmail.com>
2020-12-16 09:47:24 +0100

    esercizio finito e corretto

--- js/primary.js
+++ js/primary.js
@@ -6,26 +6,25 @@ var iscritti = ['sebastiano.lanfranchi@gmail.com','qwerty@gmail.com'];
 
  if (iscritti.includes(mail)){
    document.getElementById('outputEmail').innerText = 'Accesso completato'
+   var sceltaUtente = parseInt(prompt('inserisci un numero da 1 a 6'));
+   var sceltaPc = parseInt(Math.floor(Math.random() * 5) +1);
+   document.getElementById('scelta1').innerText = 'La tua scelta:' + sceltaUtente;
+   document.getElementById('scelta2').innerText = 'La scelta del pc:' + sceltaPc;
+   if (sceltaUtente > sceltaPc){
+    document.getElementById('outputDadi').innerText = 'Hai vinto'
+   }
+   if (sceltaUtente < sceltaPc) {
+     document.getElementById('outputDadi').innerText = 'Hai perso'
+   }
+   if (sceltaUtente === sceltaPc){
+     document.getElementById('outputDadi').innerText = 'Hai pareggiato'
+   }
+   if(sceltaUtente === 0 || sceltaUtente > 6){
+     document.getElementById('outputDadi').innerText = 'Non barare e non rompermi il codice!'
+   }
  }
 else {
   document.getElementById('outputEmail').innerText = 'Accesso negato'
 }
 
 // Esercizio 2
-
-var sceltaUtente = parseInt(prompt('inserisci un numero da 1 a 6'));
-var sceltaPc = parseInt(Math.floor(Math.random() * 5) +1);
-document.getElementById('scelta1').innerText = 'La tua scelta:' + sceltaUtente;
-document.getElementById('scelta2').innerText = 'La scelta del pc:' + sceltaPc;
-if (sceltaUtente > sceltaPc){
- document.getElementById('outputDadi').innerText = 'Hai vinto'
-}
-if (sceltaUtente < sceltaPc) {
-  document.getElementById('outputDadi').innerText = 'Hai perso'
-}
-if (sceltaUtente === sceltaPc){
-  document.getElementById('outputDadi').innerText = 'Hai pareggiato'
-}
-if(sceltaUtente === 0 || sceltaUtente > 6){
-  document.getElementById('outputDadi').innerText = 'Non barare e non rompermi il codice!'
-}
